a7bfa272efe92d29cff541ef8aef41ab3ef07756
Michael Campbell <mtcampbe@illinois.edu>
2022-05-26 19:45:21 -0500

    Update timing data label for per-step timing

--- utils/plot-timings.py
+++ utils/plot-timings.py
@@ -97,7 +97,7 @@ def main():
     figheight=8
     if args.per_step:
         timing_names =  timing_names[-1:]
-        timing_labels = ["second 9 timesteps, per time step"]
+        timing_labels = ["walltime per time step"]
         scalfac = 1.0/9.0
         figheight=4
 
