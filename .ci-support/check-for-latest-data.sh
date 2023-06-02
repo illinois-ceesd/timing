9556ac9f5247757df914a5020a2a833d5a09ca86
Michael Campbell <mtcampbe@illinois.edu>
2023-06-02 14:25:30 -0500

    Make checker work on mac too.

--- .ci-support/check-for-latest-data.sh
+++ .ci-support/check-for-latest-data.sh
@@ -4,7 +4,11 @@ dir_name=$1
 date_offset=${2:-0}
 testing_date=$(cat latest_testing_date)
 tdf=${testing_date//./-}
-test_date=$(date -d"$tdf -${date_offset} days" +%Y.%m.%d)
+if date --version >/dev/null 2>&1; then
+    test_date=$(date -d"$tdf -${date_offset} days" +%Y.%m.%d)
+else
+    test_date=$(date -j -v-${date_offset}d -f "%Y-%m-%d" "${tdf}" "+%Y.%m.%d")
+fi
 printf "Tests ran on: ${testing_date}\n"
 printf "Checking for timing data from: ${test_date}\n"
 search_pattern="${dir_name}/*${test_date}*sqlite"
