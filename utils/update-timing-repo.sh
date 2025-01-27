d5bf472fbc365ca620699095c110b47b5461ebb1
Michael T. Campbell <mtcampbe@illinois.edu>
2025-01-27 09:46:22 -0800

    Hardcode TOPDIR

--- utils/update-timing-repo.sh
+++ utils/update-timing-repo.sh
@@ -8,7 +8,7 @@
 # - Submits the y3-prediction scaling run jobs for 
 # tonight's tests 
 
-TOPDIR=$(pwd)
+TOPDIR="/p/gpfs1/mtcampbe/CEESD/AutomatedTesting/RepoMonitoring/pilot-timing"
 
 SCALING_CASE_RUN_ROOT="y3-prediction-scaling-run"
 SCALING_CASE_TIMING_ROOT="y3-prediction"
